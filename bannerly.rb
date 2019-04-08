2b1daf0f080f387e2c368aeee16dcc275bd648b7
Aaron Young <young@squareup.com>
2019-04-07 22:55:29 -0400

    Initial revision

---
+++ bannerly.rb
@@ -0,0 +1,113 @@
+#!/usr/bin/env ruby
+
+require 'optparse'
+
+class BannerData
+  def initialize(phrase)
+    @text = `figlet -f banner #{phrase}`
+  end
+  
+  def bumpCol
+    @col = @col +1
+  end
+
+  def bumpRow
+    @row = @row + 1
+    @col = 0
+  end
+
+  def printTopBorder
+    if $global_opts[:border]
+      found = false
+      count = 1
+      @text.split('').each { |c|
+        if c == '#'
+          found = true
+        elsif c == "\n"
+          break
+        end
+        count = count + 1
+      }
+
+      if found
+        (1..count).each do |i|
+          printBack
+          bumpCol
+        end
+        print "\n"
+        bumpRow
+      end
+    end
+  end
+
+  def printText
+    print ":#{$global_opts[:text]}:"
+  end
+
+  def printBack
+    if $global_opts[:width] == 0 or $global_opts[:height] == 0
+      fill = ''
+    else
+      fill = "#{@row % $global_opts[:height] + 1}#{@col % $global_opts[:width] + 1}"
+    end
+    print ":#{$global_opts[:bg]}#{fill}:"
+  end
+
+  def output
+    @row = 0
+    @col = 0
+
+    printTopBorder
+
+    @text.split('').each { |c|
+      if c == "\n"
+        bumpRow
+        print "\n"
+        next
+      end
+      
+      if $global_opts[:border] and @col == 0
+        printBack
+        bumpCol
+      end
+
+      if c == '#'
+        printText
+      elsif c == ' '
+        printBack
+      end
+      
+      bumpCol
+    }
+  end
+end
+
+$global_opts = {
+  :width => 0,
+  :height => 0,
+  :text => "partyparrot",
+  :bg => "cash",
+  :border => true,
+}
+
+OptionParser.new do |opts|
+  opts.on("--width <WIDTH>", "How wide the banner bigemoji is.") do |v|
+    $global_opts[:width] = v.to_i
+  end
+  opts.on("--height <HEIGHT>", "How high the banner bigemoji is.") do |v|
+    $global_opts[:height] = v.to_i
+  end
+  opts.on("--text <TEXT_EMOJI>", "Which emoji to use for the text.") do |v|
+    $global_opts[:text] = v.gsub(/:/, '')
+  end
+  opts.on("--bg <BG_EMOJI>", "Which emoji to use for the background.") do |v|
+    $global_opts[:bg] = v.gsub(/:/, '')
+  end
+  opts.on("--noborder", "Remove the margin.") do |v|
+    $global_opts[:border] = false
+  end
+end.parse!
+
+banner = BannerData.new(ARGV.join(" "))
+banner.output
+
---
+++ testit.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+tmpfile=$(mktemp /tmp/bannerly.result.$$)
+
+checkResult() {
+  if [ $? -eq 0 ]; then
+    echo "OK"
+  else
+    echo "FAIL"
+  fi
+}
+
+echo "stg test"
+./bannerly.rb --width 3 --height 3 --text aaa --bg stg hello >$tmpfile
+diff $tmpfile stgtest.txt
+checkResult
+
+echo "stg test no border"
+./bannerly.rb --width 3 --height 3 --text aaa --bg stg --noborder hello >$tmpfile
+diff $tmpfile stgtestnoborder.txt
+checkResult
+
+echo "cash test"
+./bannerly.rb --text aaa --bg cash hello >$tmpfile
+diff $tmpfile cashtest.txt
+checkResult
+
+echo "default test"
+./bannerly.rb hello >$tmpfile
+diff $tmpfile defaulttest.txt
+checkResult
+
+rm $tmpfile
+
